049af1a24e298370720632cec350c0f0329e9bd8
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-15 15:04:35 -0300

    # Testes Login

--- PizzariaTestes/Login/LoginTestes.cs
+++ PizzariaTestes/Login/LoginTestes.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzariaAPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace PizzariaTestes.Login
 {
-    class LoginTestes
+    public class LoginTestes
     {
+
+
+        [Fact]
+        public async Task LoginTesteSucessoAsync() {
+
+            var login = new LoginController();
+
+            IActionResult actionResult = await login.Login("lorran","123");
+
+            Assert.NotNull(actionResult);
+
+            OkObjectResult result = actionResult as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200,result.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task LoginTesteErro() {
+            var login = new LoginController();
+
+            IActionResult actionResult = await login.Login("lorran","12345");
+
+            Assert.NotNull(actionResult);
+
+            var result = actionResult as BadRequestObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(400,result.StatusCode);
+
+        }
+
     }
 }
