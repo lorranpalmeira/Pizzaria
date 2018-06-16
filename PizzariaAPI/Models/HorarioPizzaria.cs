8d1f7ee6827e951a1e8e8e84f4b3037367cf4a17
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-16 19:26:40 -0300

    # Horarios

---
+++ PizzariaAPI/Models/HorarioPizzaria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzariaAPI.Models
+{
+    public class HorarioPizzaria
+    {
+
+        public int IdPizzaria { get; set; }
+
+        public TimeSpan HorarioAbertura { get; set; }
+
+        public TimeSpan HorarioFechamento{ get; set; }
+
+
+
+    }
+}
