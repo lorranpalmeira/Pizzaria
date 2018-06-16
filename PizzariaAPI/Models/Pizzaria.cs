6d6adb5d3df0594ce2f8f249205f04e1833cc6cc
LORRAN PALMEIRA <lorranpalmeira@outlook.com>
2018-06-16 20:31:42 -0300

    #Required Pizzaria

--- PizzariaAPI/Models/Pizzaria.cs
+++ PizzariaAPI/Models/Pizzaria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,13 @@ namespace PizzariaAPI.Models
 
         public int IdPizzaria { get; set; }
 
+        [Required]
         public string Nome { get; set; }
 
+        [Required]
         public string NomeFantasia { get; set; }
 
+        [Required]
         public string Cnpj { get; set; }
 
         public bool Ativo { get; set; }
